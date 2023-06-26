cf82664fa813ef7fb0217c4fb2dd272b9548c89e
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-06-26 14:53:14 +0100

    feat: add component search

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Container } from './styles'
+import { Container, SearchButton, SearchContainer, Input } from './styles'
 import { Header } from '../../components/Header'
+import { Feather } from "@expo/vector-icons"
 
 function Home() {
   return (
      <Container>
       <Header title={"React Prime"}/>
+      <SearchContainer>
+        <Input
+          placeholder="Ex vingadores.."
+          placeholderTextColor={"#ddd"}
+        />
+        <SearchButton>
+          <Feather name="search" size={30} color="#FFF" />
+        </SearchButton>
+      </SearchContainer>
      </Container>
   )
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -6,3 +6,29 @@ export const Container = styled.SafeAreaView`
   flex: 1;
   padding: 4px 0;
 `
+
+export const SearchContainer = styled.View`
+  flex-direction: row;
+  width: 100%;
+  height: 50px;
+  align-items: center;
+  padding: 0 14px;
+  margin-bottom: 8px;
+`
+
+export const SearchButton = styled.TouchableOpacity`
+  width: 15%;
+  height: 50px;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Input = styled.TextInput`
+  background: rgba(255, 255, 255, 0.4);
+  width: 85%;
+  height: 50px;
+  border-radius: 50px;
+  padding: 8px 15px;
+  font-size: 18px;
+  color: #fff;
+`
