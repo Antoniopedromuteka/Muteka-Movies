600f647c7f61edf42095062eb178917313897809
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-06-26 13:07:07 +0100

    feat: menu drawer done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,53 @@
 import React from 'react'
 import { createDrawerNavigator } from "@react-navigation/drawer"
-import Home from '../pages/home'
 import Movies from '../pages/Movies'
+import StackRoutes from './StackRoutes'
+import { MaterialCommunityIcons } from "@expo/vector-icons"
 
 const Drawer = createDrawerNavigator()
 
 function Routes(){
   return(
-    <Drawer.Navigator>
-      <Drawer.Screen name='Home' component={Home}/>
-      <Drawer.Screen name='Movies' component={Movies}/>
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: false,
+        drawerStyle:{
+          backgroundColor: '#090A0E',
+          paddingTop: 20
+        },
+        drawerActiveBackgroundColor: '#E72F49',
+        drawerActiveTintColor: '#fff',
+        drawerInactiveBackgroundColor: '#090A0E',
+        drawerInactiveTintColor: '#fff'
+      }}
+    >
+      <Drawer.Screen
+        options={{
+          title: 'Home',
+          drawerIcon: ({focused, size, color}) =>(
+            <MaterialCommunityIcons 
+              name={focused ? 'movie-open' : 'movie-outline'}
+              size={size}
+              color={color}
+            />
+          )
+        }}
+        name='HomeDrawer'
+        component={StackRoutes}
+      />
+      <Drawer.Screen
+        options={{
+          title: 'Meus Filmes',
+          drawerIcon: ({focused, size, color}) =>(
+            <MaterialCommunityIcons 
+              name={focused? 'archive' : 'archive-outline'}
+              size={size}
+              color={color}
+            />
+        )}}
+        name='Movies'
+        component={Movies}
+      />
     </Drawer.Navigator>
   )
 }
--- src/routes/stackRoutes.js
+++ src/routes/stackRoutes.js
@@ -1,2 +1,19 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import Home from "../pages/home";
+import Movies from "../pages/Movies";
+
+
+const Stack = createNativeStackNavigator();
+
+
+function StackRoutes(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home}/>
+      {/* <Stack.Screen name="Movies" component={Movies}/> */}
+    </Stack.Navigator>
+  )
+}
+
+export default StackRoutes;
